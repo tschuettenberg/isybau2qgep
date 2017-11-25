d57d8a39bdc6664d5eb77e2cf9b2e23dcf600c79
Thomas Schüttenberg <thomas@qgis.de>
2017-11-25 18:35:04 +0100

    change schema

--- v_punktgeometrien.sql
+++ v_punktgeometrien.sql
@@ -1,8 +1,8 @@
 --Knoten: Bildung der Punktgeometrien, Achtung: nicht deckungsgleich mit Abwassertechnischen Anlagen, mehrer Punkte pro Anlage (SMP, DMP, etc.) möglich!
-CREATE OR REPLACE VIEW sta_san_abn.v_objektgeometrien_p AS 
+CREATE OR REPLACE VIEW isy_in.v_objektgeometrien_p AS 
  SELECT kp.ogc_fid, kp.ogr_pkid, kp.parent_ogr_pkid,
     kp.rechtswert, kp.hochwert, kp.punkthoehe,
     kp.punktattributabwasser,
     kp.lagegenauigkeitsstufe, kp.hoehengenauigkeitsstufe,
     ST_SetSRID(ST_MakePoint(kp.rechtswert, kp.hochwert), 25832)::geometry(Point,25832) AS p_geom_2d
-   FROM sta_san_abn.identi_datenk_stammd_abwassanlage_geomet_geomet_knoten_punkt kp;
+   FROM isy_in.identi_datenk_stammd_abwassanlage_geomet_geomet_knoten_punkt kp;
